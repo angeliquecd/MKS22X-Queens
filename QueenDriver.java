b826825db5a0ab99732498f4864408908d5b1748
angeliquecd <acharles-davis00@stuy.edu>
2019-02-08 10:04:52 -0500

    started a driver

---
+++ QueenDriver.java
@@ -0,0 +1,7 @@
+ public class QueenDriver{
+   public static void main(String[] args){
+     QueenBoard tester = new QueenBoard(2);
+     System.out.println(tester);
+     System.out.println(tester.solve());
+   }
+ }
